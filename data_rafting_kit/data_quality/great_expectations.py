4d4ea2bcb3644ff609569c1cda1366d9e66fbfe0
Ben Constable <benconstable@microsoft.com>
2024-06-17 15:15:48 +0000

    Update column

--- data_rafting_kit/data_quality/great_expectations.py
+++ data_rafting_kit/data_quality/great_expectations.py
@@ -42,10 +42,11 @@ GREAT_EXPECTATIONS_DYNAMIC_DATA_QUALITY = [
 STANDARD_ARG_TYPES = {
     "mostly": (float | None, Field(default=1.0, alias="mostly")),
     "column": (str, Field(required=True, alias="column")),
-    "column_a": (str, Field(required=True, alias="column_a")),
-    "column_b": (str, Field(required=True, alias="column_b")),
-    "type": (str, Field(required=True, alias="type")),
+    "column_A": (str, Field(required=True, alias="column_a")),
+    "column_B": (str, Field(required=True, alias="column_b")),
+    "type_": (str, Field(required=True, alias="type")),
     "values": (list[str | int | float], Field(required=True, alias="values")),
+    "value_set": (list[str | int | float], Field(required=True, alias="values")),
     "value": (int, Field(required=True, alias="value")),
     "min_value": (
         float | datetime | str | None,
@@ -67,22 +68,25 @@ STANDARD_ARG_TYPES = {
         Literal["both_values_are_missing", "either_value_is_missing", "neither"],
         Field(default="both_values_are_missing", alias="ignore_row_if"),
     ),
-    "value_pairs": (list[tuple[Any, Any]], Field(required=True, alias="value_pairs")),
+    "value_pairs": (list[tuple[Any, Any]], Field(required=True, alias="values")),
+    "value_pairs_set": (list[tuple[Any, Any]], Field(required=True, alias="values")),
     "column_index": (int | None, Field(default=None, alias="column_index")),
     "double_sided": (bool, Field(required=True, alias="double_sided")),
     "threshold": (int | float, Field(required=True, alias="threshold")),
     "strictly": (bool | None, Field(default=None, alias="strictly")),
-    "types": (list[str], Field(required=True, alias="types")),
+    "type_list": (list[str], Field(required=True, alias="types")),
     "json_schema": (str, Field(required=True, alias="json_schema")),
     "like_pattern": (str, Field(required=True, alias="like_pattern")),
     "match_on": (Literal["any", "all"] | None, Field(default="any", alias="match_on")),
-    "like_patterns": (list[str], Field(required=True, alias="like_patterns")),
-    "regex": (str | list[str], Field(required=True, alias="regex")),
+    "like_pattern_list": (list[str], Field(required=True, alias="like_patterns")),
+    "regex": (str | str, Field(required=True, alias="regex")),
+    "regex_list": (str | list[str], Field(required=True, alias="regexs")),
     "strftime_format": (str, Field(required=True, alias="strftime_format")),
-    "columns": (list[str], Field(required=True, alias="columns")),
+    "column_list": (list[str], Field(required=True, alias="columns")),
     "sum_total": (float | int, Field(required=True, alias="sum_total")),
     "exact_match": (bool | None, Field(default=True, alias="exact_match")),
-    "column_set": (list[str], Field(required=True, alias="column_set")),
+    "column_set": (list[str], Field(required=True, alias="columns")),
+    "column_values": (list[str], Field(required=True, alias="columns")),
 }
 
 EXCLUDED_ARG_TYPES = ["auto", "profiler_config", "allow_cross_type_comparisons"]
@@ -95,45 +99,6 @@ class GreatExpectationBaseSpec(DataQualityBaseSpec):
     pass
 
 
-GREAT_EXPECTATIONS_DYNAMIC_DATA_QUALITY_PARAMATER_REPLACEMENT_MAP = {
-    "expect_table_columns_to_match_ordered_list": {"column_list": "columns"},
-    "expect_table_columns_to_match_set": {"column_set": "columns"},
-    "expect_compound_columns_to_be_unique": {"column_list": "columns"},
-    "expect_multicolumn_sum_to_equal": {"column_list": "columns"},
-    "expect_select_column_values_to_be_unique_within_record": {
-        "column_list": "columns"
-    },
-    "expect_column_values_to_not_match_regex_list": {"regex_list": "regex"},
-    "expect_column_values_to_match_regex_list": {"regex_list": "regex"},
-    "expect_column_values_to_not_be_in_set": {"value_set": "values"},
-    "expect_column_values_to_be_in_set": {"value_set": "values"},
-    "expect_column_distinct_values_to_be_in_set": {"value_set": "values"},
-    "expect_column_distinct_values_to_contain_set": {"value_set": "values"},
-    "expect_column_distinct_values_to_equal_set": {"value_set": "values"},
-    "expect_column_most_common_value_to_be_in_set": {"value_set": "values"},
-    "expect_column_values_to_not_match_like_pattern_list": {
-        "like_pattern_list": "like_patterns"
-    },
-    "expect_column_values_to_match_like_pattern_list": {
-        "like_pattern_list": "like_patterns"
-    },
-    "expect_column_values_to_be_in_type_list": {"type_list": "types"},
-    "expect_column_pair_values_to_be_equal": {
-        "column_A": "column_a",
-        "column_B": "column_b",
-    },
-    "expect_column_pair_values_a_to_be_greater_than_b": {
-        "column_A": "column_a",
-        "column_B": "column_b",
-    },
-    "expect_column_pair_values_to_be_in_set": {
-        "column_A": "column_a",
-        "column_B": "column_b",
-        "value_pairs_set": "value_pairs",
-    },
-    "expect_column_values_to_be_of_type": {"type_": "type"},
-}
-
 dynamic_great_expectations_data_quality_models = []
 for expectation_name in GREAT_EXPECTATIONS_DYNAMIC_DATA_QUALITY:
     param_fields = {}
@@ -143,22 +108,10 @@ for expectation_name in GREAT_EXPECTATIONS_DYNAMIC_DATA_QUALITY:
     for arg in arg_keys:
         if arg in EXCLUDED_ARG_TYPES:
             continue
+        elif arg in STANDARD_ARG_TYPES:
+            param_fields[arg] = STANDARD_ARG_TYPES[arg]
         else:
-            try:
-                cleaned_arg = (
-                    GREAT_EXPECTATIONS_DYNAMIC_DATA_QUALITY_PARAMATER_REPLACEMENT_MAP[
-                        expectation_name
-                    ][arg]
-                )
-            except KeyError:
-                cleaned_arg = arg
-
-            if cleaned_arg in STANDARD_ARG_TYPES:
-                param_fields[arg] = STANDARD_ARG_TYPES[cleaned_arg]
-            else:
-                raise ValueError(
-                    f"Couldn't find type for {cleaned_arg} in {expectation_class}"
-                )
+            raise ValueError(f"Couldn't find type for {arg} in {expectation_class}")
 
     param_config = ConfigDict(allow_population_by_field_name=True)
     dynamic_data_quality_param_model = create_model(
@@ -277,6 +230,7 @@ class GreatExpectationsDataQuality(DataQualityBase):
                     filter_expression_pattern,
                     result.result["unexpected_index_query"],
                 ).group(1)
+                print(filter_expression)
                 fixed_filter_expression = self.fix_unquoted_strings(filter_expression)
                 filter_expressions.append(f"({fixed_filter_expression})")
             else:
